f74266fb7c735f761dccbe5b986eb270d38400f7
Nedqlkov <fbinnzhivko@gmail.com>
2017-06-03 23:01:08 +0300

    Finish first tree excercises

--- 06.Exercise DATA DEFINITION AND DATATYPES/zaqvki.sql
+++ 06.Exercise DATA DEFINITION AND DATATYPES/zaqvki.sql
@@ -80,31 +80,278 @@ MODIFY  COLUMN last_login_time
 TIMESTAMP 
 NOT NULL DEFAULT CURRENT_TIMESTAMP;
 -- -----------11------------------
-
+ALTER TABLE users MODIFY id BIGINT NOT NULL;
+ALTER TABLE users
+DROP PRIMARY KEY;
+ALTER TABLE users
+ADD CONSTRAINT pk_users PRIMARY KEY(id);
+ALTER TABLE users
+ADD CONSTRAINT uq_username UNIQUE (username);
 -- -----------12------------------
+CREATE TABLE directors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    director_name VARCHAR(30) not null,
+    notes BLOB
+);
+insert into directors(id,director_name)
+values(1,'dasdasd'),(2,'dasdasd'),(3,'dasdasd'),(4,'dasdasd'),(5,'dasdasd');
 
+CREATE TABLE genres (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    genre_name VARCHAR(30) not null not null,
+    notes BLOB
+);
+insert into genres(id,genre_name)
+values(2,'dasdad'),(1,'dasdad'),(3,'dasdad'),(4,'dasdad'),(5,'dasdad');
+
+
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(30) not null,
+    notes BLOB
+);
+insert into categories(id,category_name)values(1,'wi-fi')
+,(2,'wi-fi')
+,(3,'wi-fi')
+,(4,'wi-fi')
+,(5,'wi-fi');
+
+CREATE TABLE movies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) not null,
+    director_id INT,
+    copyright_year DATETIME not null,
+    length INT not null,
+    genre_id INT not null,
+    category_id INT not null,
+    rating INT,
+    notes BLOB
+);
+ insert into movies(id,title,copyright_year,`length`,genre_id,category_id)
+ values(11,'dasdasda','2016-12-12',23,1,2),
+(10,'dasdasda','2016-12-12',23,1,2),
+(13,'dasdasda','2016-12-12',23,1,2),
+(14,'dasdasda','2016-12-12',23,1,2),
+(15,'dasdasda','2016-12-12',23,1,2);
 -- -----------13------------------
+create table `categories`(
+   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   category varchar(50) not null,
+   daily_rate decimal(5,2),
+   weekly_rate decimal(5,2),
+   monthly_rate decimal(5,2),
+   weekend_rate decimal(5,2)
+);
+insert into categories(category)
+values('dasdads'),('dasdads'),('dasdads'),('dasdads'),('dasdads');
+
+create table `cars`(
+   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   plate_number int not null,
+   make varchar(10) not null,
+   car_year datetime,
+   category_id int,
+   doors INT,
+   picture blob,
+   car_condition varchar(20),
+   available bit
+);
+insert into `cars`(plate_number,make) values(4,'dasd'),(4,'dasd'),(4,'dasd'),(4,'dasd'),(4,'dasd');
+
+create table `employees`(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name varchar(50) not null,
+    last_name varchar(50),
+    title varchar(50),
+    notes TEXT
+);
+insert into `employees`(first_name) values('dasdads'),('dasdads'),('dasdads'),('dasdads'),('dasdads');
+
+create table`customers`(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  driver_licence_number varchar(20),
+  full_name varchar(50),
+  address varchar(50),
+  city varchar(50) not null,
+  zip_code varchar(50),
+  notes TEXT
+);
+insert into `customers`(city) values('dasdads'),('dasdads'),('dasdads'),('dasdads'),('dasdads');
+
+create table`rental_orders`(
+   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   employee_id int,
+   customer_id int,
+   car_id int,
+   car_condition varchar(50), 
+   tank_level decimal (20,2),
+   kilometrage_start int,
+   kilometrage_end int,
+   total_kilometrage int,
+   start_date datetime,
+   end_date datetime,
+   total_days int,
+   rate_applied varchar(50),
+   tax_rate int,
+   order_status VARCHAR(30),
+   notes TEXT
+);
+insert into `rental_orders`(employee_id,customer_id,car_id) values(1,2,1),(2,2,1),(3,2,1),(4,2,1),(5,2,1);
+
 
 -- -----------14------------------
+create table employees(
+   id INT,
+  first_name varchar(50) not null,
+  last_name varchar(50),
+  title varchar(50),
+  notes blob
+  ,PRIMARY KEY (id)
+);
+insert into employees(id,first_name)values(1,'dasd'),(2,'dasd'),(3,'dasd');
 
+create table customers (
+   account_number int not null,
+   first_name varchar(50) not null, 
+	last_name varchar(50),
+	phone_number varchar(10),
+	emergency_name varchar(50),
+	emergency_number varchar(10),
+	notes blob,
+	primary key(account_number)
+);
+insert into customers(account_number,first_name)values(1,'dasd'),(2,'dasd'),(3,'dasd');
+create table room_status (
+   room_status int not null,
+   notes blob,
+   primary key(room_status)
+);
+insert into room_status(room_status)values(1),(2),(3);
+
+create table room_types(
+   room_type INT not null,
+   notes blob not null,
+   primary key(room_type)
+);
+insert into room_types(room_type,notes)values(1,'sasas'),(2,'sasas'),(3,'sasasa');
+
+create table bed_types(
+  bed_type INT not null,
+  notes blob not null,
+  primary key(bed_type)
+);
+insert into bed_types(bed_type,notes)values(1,'sasas'),(2,'sasas'),(3,'sasasa');
+
+create table rooms(
+   room_number int not null, 
+   room_type varchar(50),
+   bed_type varchar(50),
+   rate varchar(50), 
+   room_status varchar(50), 
+   notes blob not null, 
+   primary key(room_number)
+);
+insert into rooms(room_number,notes)values(1,'sasas'),(2,'sasas'),(3,'sasasa');
+
+create table payments(
+   id INT not null,
+   employee_id int,
+   payment_date datetime, 
+   account_number varchar(10), 
+   first_date_occupied datetime, 
+   last_date_occupied datetime, 
+   total_days INT, 
+   amount_charged double(10,2), 
+   tax_rate double(10,2), 
+   tax_amount double(10,2), 
+   payment_total double(10,2), 
+   notes blob not null, 
+   primary key(id)
+);
+insert into payments(id,notes)values(1,'sasas'),(2,'sasas'),(3,'sasasa');
+
+create table occupancies(
+  id int, 
+  employee_id int ,
+  date_occupied datetime,
+  account_number varchar(10),
+  room_number int,
+  rate_applied varchar(10), 
+  phone_charge varchar(10), 
+  notes blob not null, 
+  primary key(id)
+);
+insert into occupancies(id,notes)values(1,'sasas'),(2,'sasas'),(3,'sasasa');
 -- -----------15------------------
+create database soft_unii;
+use soft_unii;
+create table towns(
+   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   name varchar(30)
+);
+create table addresses(
+   id  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   address_text  varchar(50),
+   town_id INT,
+   CONSTRAINT fk_addresses_towns FOREIGN KEY (town_id)
+   REFERENCES towns(id) 
+);
+create table `departments`(
+   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   name varchar(10)
+);
+create table employees(
+   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   first_name varchar(10),
+   middle_name varchar(10),
+   last_name varchar(10),
+   job_title varchar(10),
+   department_id INT,
+   hire_date datetime,
+   salary double (10,2),
+   address_id INT,
+   CONSTRAINT FK_address_id FOREIGN KEY (address_id)
+   REFERENCES addresses(id),
+   CONSTRAINT FK_department_id FOREIGN KEY (department_id)
+   REFERENCES  departments (id)  
+);
 
 -- -----------16------------------
 
 -- -----------17------------------
+insert into towns(name)values
+('Sofia'),('Plovdiv'),('Varna'),('Burgas');
 
+insert into departments(name)values
+('Engineering'),('Sales'),('Marketing'),('Software Development'),('Quality Assurance');
+
+
+insert into employees(first_name,middle_name,last_name,job_title,department_id,hire_date,salary)
+values('Ivan','Ivanov','Ivanov','.NET Developer',4,'2013-02-01','3500.00'),
+('Petar','Petrov','Petrov','Senior Engineer',1,'2004-03-02','4000.00'),
+('Maria','Petrova','Ivanova','Intern',5,'2016-08-28','525.25'),
+('Georgi','Terziev','Ivanov','CEO',2,'2007-12-09','3000.00'),
+('Peter','Pan','Pan','Intern',3,'2016-08-28','599.88');
 -- -----------18------------------
-
+select*from towns;
+select*from departments;
+select*from employees;
 -- -----------19------------------
-
+select*from towns as t order by t.name ;
+select*from departments as d order by d.name ;
+select*from employees order by salary desc;
 -- -----------20------------------
-
+select name from towns order by name;
+select name from departments order by name;
+select first_name,last_name,job_title,salary from employees order by salary desc;
 -- -----------21------------------
-
+update employees
+set salary=salary*1.1;
+select salary from employees;
 -- -----------22------------------
-
+select tax_rate*0.97 from payments;
 -- -----------23------------------
-
+delete from occupancies;
 -- -----------24------------------
 
 
--- 10.EXERCISE BUILT-IN FUNCTIONS/zaqvki.sql
+++ 10.EXERCISE BUILT-IN FUNCTIONS/zaqvki.sql
@@ -1,20 +1,20 @@
 -- -------------------------1---------
 select first_name,last_name from Employees
 where first_name like 'SA%'
-order by first_name desc
+order by first_name desc;
 -- -------------------------2---------
 select first_name,last_name from Employees
-where last_name like '%ei%'
+where last_name like '%ei%';
 -- -------------------3--------------------
 select first_name from Employees
-where (department_id=10 or department_id=3) and (year(hire_date)>=1995 and year(hire_date)<=2005)
+where (department_id=10 or department_id=3) and (year(hire_date)>=1995 and year(hire_date)<=2005);
 -- -------------------4-----
 SELECT first_name,last_name FROM employees
 WHERE job_title not like '%engineer%';
 -- -----------------5-------------
 SELECT name FROM towns
 where length(name)=5 or length(name)=6
-order by name
+order by name;
 -- -----------------6-------------
 SELECT town_id,name FROM towns
 where name like'M%' or name like'K%' or name like'B%' or name like'E%'
@@ -36,39 +36,50 @@ from countries
 where lower(LENGTH(country_name) - LENGTH(REPLACE(country_name, 'a', ''))+1) >= 3
 order by iso_code;
 -- -----------11------------------
-
+select p.peak_name,r.river_name,lower(concat(p.peak_name,substring(r.river_name,2)))as mix
+from peaks as p,rivers as r
+where right(p.peak_name,1) = left(r.river_name,1)
+order by mix;
 -- -----------12------------------
 select name,
 DATE_FORMAT(start, '%Y-%m-%d') as start 
 from games
 where YEAR(start)=2011 or YEAR(start)=2012
-limit 50
+limit 50;
 -- -----------13------------------
 select user_name,
 SUBSTRING_INDEX(email,'@',-1) as `Email Provider`
 from users
-order by `Email Provider`,user_name
+order by `Email Provider`,user_name;
 -- -----------14------------------
 select user_name,ip_address
 from users
 where ip_address like '___.1%.%.___'
-order by user_name
-;
+order by user_name;
 -- -----------15------------------
-
+select name as Game,
+case when hour(start) between 0 and 11 then 'Morning'
+     when hour(start) between 12 and 17 then 'Afternoon'
+     when hour(start) between 18 and 23 then 'Evening'
+end as 'Part of the day',
+case when duration <=3 then 'Extra Short'
+      when duration between 4 and 6 then 'Short'
+      when duration between 7 and 10 then 'Long'
+      else 'Extra Long' end as 'Duration'
+from games;
 -- -----------16------------------
 select product_name,
 order_date,
 DATE_ADD(order_date, INTERVAL 3 DAY)   as pay_due,
 DATE_ADD( order_date, INTERVAL 1 month )  as deliver_due
-from orders
+from orders;
 -- -----------17-----------------
 select name,
 TIMESTAMPDIFF(YEAR,birthdate,CURDATE()) AS age_in_years,
 MONTH(birthdate) as age_in_months,
 datediff(birthdate, CURDATE()) as age_in_days,
 TIMESTAMPDIFF(SECOND, birthdate, CURDATE()) as age_in_minutes
-from People
+from People;
 
 
 
